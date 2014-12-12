0bda04014be0730f0a559c51342d24a4652566e6
leopoldkwok <leopoldkwok@gmail.com>
2014-12-12 15:15:11 +0000

    catered for a strike in score method

--- lib/game.rb
+++ lib/game.rb
@@ -14,22 +14,27 @@ class Game
 		end
 	end
 
-	def score 
-		score = 0
-		first_in_frame = 0
-		frame = 0
-		while frame < 10
-			if @rolls[first_in_frame] + @rolls[first_in_frame+1] == 10 #spare
-			score += 10 + @rolls[first_in_frame+2]
-				first_in_frame+=2
-			else
-				score+=@rolls[first_in_frame] + @rolls[first_in_frame+1]
-				first_in_frame+=2
-			end
-			frame +=1
-		end
-		score
-	end
+      def score
+      score = 0
+      first_in_frame = 0
+      frame = 0
+      while frame < 10
+        if @rolls[first_in_frame]== 10 #strike
+        	score +=10 + @rolls[first_in_frame + 1]+ @rolls[first_in_frame+2]
+        	first_in_frame+=1
+        elsif spare?(first_in_frame)
+          score += 10 + @rolls[first_in_frame + 2]
+          first_in_frame += 2
+        else
+        score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
+        first_in_frame += 2
+       end
+       frame +=1
+     end
+     score
+   end
+
+
 
 	def spare?(first_in_frame)
 		@rolls[first_in_frame] + @rolls[first_in_frame+1]==10
@@ -38,6 +43,10 @@ class Game
 	def roll_spare
 		roll(5)
 		roll(5)
+	end
+
+	def strike?(first_in_frame)
+		@rolls[first_in_frame] == 10
 	end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -29,4 +29,12 @@ describe Game do
 		game.roll(3)
 		game.roll_many(17,0)
 	end
+
+	it 'can roll a strike' do 
+		game.roll(10) #strike
+		game.roll(3)
+		game.roll(4)
+		game.roll_many(16,0)
+		game.score.should == 24
+	end
 end

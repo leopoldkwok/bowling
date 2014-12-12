c56d9bedf75fed591998acc26979b4ec3e17a688
leopoldkwok <leopoldkwok@gmail.com>
2014-12-12 15:39:42 +0000

    extracted methods strike_bonus_balls, spare_bonus_ball and two_balls_in_frame

--- lib/game.rb
+++ lib/game.rb
@@ -19,14 +19,14 @@ class Game
       first_in_frame = 0
       frame = 0
       while frame < 10
-        if @rolls[first_in_frame]== 10 #strike
-        	score +=10 + @rolls[first_in_frame + 1]+ @rolls[first_in_frame+2]
+        if strike?(first_in_frame)
+        	score +=10 + strike_bonus_balls(first_in_frame)
         	first_in_frame+=1
         elsif spare?(first_in_frame)
-          score += 10 + @rolls[first_in_frame + 2]
+          score += 10 + spare_bonus_ball(first_in_frame)
           first_in_frame += 2
         else
-        score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
+        score += two_balls_in_frame(first_in_frame)
         first_in_frame += 2
        end
        frame +=1
@@ -49,4 +49,15 @@ class Game
 		@rolls[first_in_frame] == 10
 	end
 
+	def strike_bonus_balls(first_in_frame)
+		@rolls[first_in_frame+1]+@rolls[first_in_frame+2]
+	end
+
+	def spare_bonus_ball(first_in_frame)
+		@rolls[first_in_frame+2]
+	end
+
+	def two_balls_in_frame(first_in_frame)
+		@rolls[first_in_frame] + @rolls[first_in_frame + 1]
+	end
 end
